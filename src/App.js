ccfa613294718fa186cf4ed9680afc1d3c77e838
Sandeep Patil <sandeep@example.com>
2025-04-11 13:06:06 +0530

    Add base App.js layout

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-<REACT_CODE>
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Brainwaves Liquidation Dashboard</h1>
+      <p>App is running successfully 🎉</p>
+    </div>
+  );
+}
+
+export default App;
